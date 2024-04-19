1f6ca4f8fc04f206b3f4cbc922b30300b1e60af7
GamingwithPro83 <114410171+GamingwithPro83@users.noreply.github.com>
2024-04-19 13:15:40 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let form = document.getElementById("SubForm")
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault()
+
+	let email = document.getElementById("email").value
+	let phone = document.getElementById("phone").value
+	let address_location = document.getElementById("address-location").value
+	let dump_location = document.getElementById("dump-location").value
+	console.log(email, phone, address_location, dump_location)
+
+	fetch("http://127.0.0.1:5000/subscribe", {
+		method: "POST",
+		body: JSON.stringify({ email: email, phone: phone, address_location: address_location, dump_location: dump_location }),
+	})
+		.then((data) => data.json())
+		.then((data) => console.log(data))
+})
